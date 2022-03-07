5da52fcfb903b68d424fa7ce9749509cde0f4314
Luke Lavan <luke.lavan01@gmail.com>
2022-03-06 19:48:39 -0500

    add gear tab

--- system/fvtt-2neal.mjs
+++ system/fvtt-2neal.mjs
@@ -4,6 +4,7 @@ import {TwoDotNealActorSheet} from './actor-sheet.mjs';
 /* console.log Handlebars helper */
 Handlebars.registerHelper('log', (x) => console.log(x));
 
+/* FoundryVTT hooks */
 Hooks.once('init', async function () {
     console.log('Initializing 2.Neal system');
 
