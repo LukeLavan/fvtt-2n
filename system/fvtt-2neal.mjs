38ab5c7aa7daa496795840a10cd381ea8cfb65b9
Luke Lavan <luke.lavan01@gmail.com>
2022-06-15 20:13:33 -0400

    add option to hide certain item types from item creation dialog

--- system/fvtt-2neal.mjs
+++ system/fvtt-2neal.mjs
@@ -26,6 +26,19 @@ Hooks.once('init', async function () {
     //Actors.registerSheet('fvtt-2neal', TwoDotNealItemSheet, {
     //    makeDefault: true,
     //});
+
+    game.settings.register('fvtt-2neal', 'createItemTypesHidden', {
+        name: 'Hidden Item Types',
+        hint: 'Hides items in create item dialog',
+        scope: 'client',
+        config: true,
+        type: String,
+        default:
+            'throwMod, acMod, hitMod, gearTab, nonWeaponProficiency, weaponProficiency',
+        onChange: (value) => {
+            console.log(value);
+        },
+    });
 });
 
 // make rolls show dice automatically
@@ -35,4 +48,28 @@ Hooks.on('renderChatMessage', function (message) {
             `li.chat-message[data-message-id="${message.id}"] div.dice-tooltip`
         ).css('display', 'block');
     }, 250);
+});
+
+// restrict types of 'create new item' dialog
+Hooks.on('renderDialog', function (dialog, html) {
+    if (dialog.data.title === 'Create New Item') {
+        let content = html[0].innerHTML;
+        const types = game.settings
+            .get('fvtt-2neal', 'createItemTypesHidden')
+            .split(',');
+
+        // remove selected attribute
+        content = content.replace(' selected=""', '');
+
+        // remove options
+        types.map((type) => {
+            type = type.trim();
+            content = content.replace(
+                '<option value="' + type + '">' + type + '</option>',
+                ''
+            );
+        });
+
+        html[0].innerHTML = content;
+    }
 });
